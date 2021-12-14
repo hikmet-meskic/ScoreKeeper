b7ad36d4c7d97ceb1efae69fa29238da050e4072
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-14 21:46:00 +0100

    Add click event listeners and manipulate DOM

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Getting elements from HTML DOM
 const result1 = document.querySelector('#result1');
 const result2 = document.querySelector('#result2');
 const playingTo = document.querySelector('#score-select');
@@ -6,3 +7,31 @@ const btnPlayerOne = document.querySelector('#playerOne');
 const btnPlayerTwo = document.querySelector('#playerTwo');
 const btnReset = document.querySelector('#reset');
 
+let score1 = 0;
+let score2 = 0;
+
+// Function for reseting all application parameters
+const reset = () => {
+    score1 = 0;
+    score2 = 0;
+    result1.innerHTML = score1;
+    result2.innerHTML = score2;
+};
+
+// Adding event listeners for buttons
+btnPlayerOne.addEventListener('click', (e) => {
+    ++score1;
+    result1.innerHTML = score1;
+
+    if (score1 === parseInt(playingTo.options[playingTo.selectedIndex].text)) {
+        console.log(e);
+    }
+});
+
+btnPlayerTwo.addEventListener('click', (e) => {
+    ++score2;
+    result2.innerHTML = score2;
+});
+
+// Reset button
+btnReset.addEventListener('click', reset);
