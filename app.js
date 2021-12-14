708ff259f4d2afd2d096720e3b0ba340ca4faf1b
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-14 21:11:38 +0100

    Get elements from HTML in JS file

---
+++ app.js
@@ -0,0 +1,8 @@
+const result1 = document.querySelector('#result1');
+const result2 = document.querySelector('#result2');
+const playingTo = document.querySelector('#score-select');
+
+const btnPlayerOne = document.querySelector('#playerOne');
+const btnPlayerTwo = document.querySelector('#playerTwo');
+const btnReset = document.querySelector('#reset');
+
