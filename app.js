6c5a427f326ceb602403f307d3def22a22eb2859
Hikmet Meskic <hikmet.meskic2018@size.ba>
2021-12-15 14:47:58 +0100

    Refactor code, add two objects to hold properties

--- app.js
+++ app.js
@@ -1,75 +1,81 @@
-// Getting elements from HTML DOM
-const result1 = document.querySelector('#result1');
-const result2 = document.querySelector('#result2');
+// Setting two objects with appropriate properties
+const player1 = {
+    score: 0,
+    result: document.querySelector('#result1'),
+    btnPlayer: document.querySelector('#playerOne'),
+    playerName: 'Player One'
+}
+const player2 = {
+    score: 0,
+    result: document.querySelector('#result2'),
+    btnPlayer: document.querySelector('#playerTwo'),
+    playerName: 'Player Two'
+}
+
 const playingTo = document.querySelector('#score-select');
-
-const btnPlayerOne = document.querySelector('#playerOne');
-const btnPlayerTwo = document.querySelector('#playerTwo');
 const btnReset = document.querySelector('#reset');
-
+let won = false;
 // Using prompt box to get input from users
-const player1Name = prompt('Please enter your name\nFirst player', 'Player One');
-const player2Name = prompt('Please enter your name\nSecond player', 'Player Two');
-btnPlayerOne.innerText = `+1 ${player1Name}`;
-btnPlayerTwo.innerText = `+1 ${player2Name}`;
-
-let score1 = 0;
-let score2 = 0;
+function inputPlayerNames() {
+    player1.playerName = prompt('Please enter your name\nFirst player', 'Player One');
+    player2.playerName = prompt('Please enter your name\nSecond player', 'Player Two');
+    player1.playerName.innerText = `+1 ${player1.playerName}`;
+    player2.playerName.innerText = `+1 ${player2.playerName}`;
+}
+inputPlayerNames();
 
 // Function for reseting all application parameters
-const reset = () => {
-    score1 = 0;
-    score2 = 0;
-    result1.innerHTML = score1;
-    result2.innerHTML = score2;
-    result1.style.color = 'azure';
-    result2.style.color = 'azure';
-    btnPlayerOne.disabled = false;
-    btnPlayerTwo.disabled = false;
-    btnPlayerOne.style.cursor = 'pointer';
-    btnPlayerTwo.style.cursor = 'pointer';
-    document.body.removeChild(document.body.lastChild);
+function reset() {
+    player1.score = 0;
+    player2.score = 0;
+    player1.result.innerHTML = player1.score;
+    player2.result.innerHTML = player2.score;
+    player1.result.style.color = 'azure';
+    player2.result.style.color = 'azure';
+    player1.btnPlayer.disabled = false;
+    player2.btnPlayer.disabled = false;
+    player1.btnPlayer.style.cursor = 'pointer';
+    player2.btnPlayer.style.cursor = 'pointer';
+    if(won) {
+        document.body.removeChild(document.body.lastChild);
+        won = false;
+    }
 }
 
 // Function for disabling buttons 
-const disableButtons = () => {
-    btnPlayerOne.disabled = true;
-    btnPlayerTwo.disabled = true;
-    btnPlayerOne.style.cursor = 'not-allowed';
-    btnPlayerTwo.style.cursor = 'not-allowed';
+function disableButtons() {
+    player1.btnPlayer.disabled = true;
+    player2.btnPlayer.disabled = true;
+    player1.btnPlayer.style.cursor = 'not-allowed';
+    player2.btnPlayer.style.cursor = 'not-allowed';
 }
 
-const addWinner = (winner) => {
+function addWinner(winner) {
     let h1 = document.createElement('h1');
     h1.innerHTML = `${winner} won the game!`;
     h1.style.textAlign = 'center';
     h1.style.color = 'gold';
     h1.style.textDecoration = 'underline dotted';
     document.body.append(h1);
+    won = true;
 }
+function updateScores(player, opponent) {
+    ++player.score;
+    player.result.innerHTML = player.score;
 
+    if (player.score === parseInt(playingTo.options[playingTo.selectedIndex].text)) {
+        player.result.style.color = 'gold';
+        opponent.result.style.color = 'black';
+        disableButtons();
+        addWinner(player.playerName);
+    }
+}
 // Adding event listeners for buttons
-btnPlayerOne.addEventListener('click', (e) => {
-    ++score1;
-    result1.innerHTML = score1;
-
-    if (score1 === parseInt(playingTo.options[playingTo.selectedIndex].text)) {
-        result1.style.color = 'gold';
-        result2.style.color = 'black';
-        disableButtons();
-        addWinner(player1Name);
-    }
+player1.btnPlayer.addEventListener('click', (e) => {
+    updateScores(player1, player2);
 });
-btnPlayerTwo.addEventListener('click', (e) => {
-    ++score2;
-    result2.innerHTML = score2;
-
-    if (score2 === parseInt(playingTo.options[playingTo.selectedIndex].text)) {
-        result2.style.color = 'gold';
-        result1.style.color = 'black';
-        disableButtons();
-        addWinner(player2Name);
-    }
+player2.btnPlayer.addEventListener('click', (e) => {
+    updateScores(player2, player1);
 });
 
 // Elements that reset the game
